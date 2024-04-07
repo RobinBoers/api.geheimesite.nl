1d6a931859208bd08f158bb5c8b14587633d1a25
Robin Boers <robindev2019@outlook.com>
2024-04-07 21:54:08 +0200

    Add webmention endpoint

---
+++ webmentions.php
@@ -0,0 +1,44 @@
+<?php
+
+#
+# Webmention endpoint
+#
+
+include "config.php";
+include "connection.php";
+
+$API_ROOT = "/webmentions";
+
+if (!isset($_POST['source'])) {
+  header($_SERVER['SERVER_PROTOCOL'] . ' 400 Bad Request');
+  echo "Missing 'source' parameter.";
+  exit;
+}
+
+if (!isset($_POST['target'])) {
+  header($_SERVER['SERVER_PROTOCOL'] . ' 400 Bad Request');
+  echo "Missing 'target' parameter.";
+  exit;
+}
+
+ob_start();
+$ch = curl_init($_POST['source']);
+curl_setopt($ch, CURLOPT_USERAGENT, $USER_AGENT);
+curl_setopt($ch, CURLOPT_HEADER,0);
+$ok = curl_exec($ch);
+curl_close($ch);
+$source = ob_get_contents();
+ob_end_clean();
+
+header($_SERVER['SERVER_PROTOCOL'] . ' 202 Accepted');
+
+if (!stristr($source, $_POST['target'])) {
+  exit;
+}
+
+$headers = "From: " . $PUBLIC_EMAIL;
+$subject = "Hooray! New webmention on <" . $_POST['source'] . ">";
+$message = "Your page <" . $_POST['target'] . "> was mentioned in <" . $_POST['source'] . ">." . "\n\r" . 
+  "I guess you're not that unpopular after all (yay!)";
+
+mail($PRIVATE_EMAIL, $subject, $message, $headers);
